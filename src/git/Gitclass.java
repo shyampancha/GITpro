664dd0cb230cb4f86858b8e00a41a176b501214a
Admin <Admin@DESKTOP-KSLGVQH>
2021-11-23 15:51:13 +0530

    i created this project

---
+++ src/git/Gitclass.java
@@ -0,0 +1,12 @@
+package git;
+
+public class Gitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello git");
+		System.out.println("stagging");
+
+	}
+
+}
